b264437b7c37b990e9d1a10f9615a81d753507be
khokan <khokancrmt@gmail.com>
2025-01-20 23:57:28 +0600

    updated data

--- src/deshboard/AdminProfile.jsx
+++ src/deshboard/AdminProfile.jsx
@@ -1,9 +1,21 @@
+import { useContext } from "react";
+import AuthContext from "../provider/AuthContext";
 
 
 const AdminProfile = () => {
+    const {user} = useContext(AuthContext);
+    console.log(user)
     return (
         <div>
-            Admin profile
+            <div>
+                <img src={user?.photoURL} alt="" />
+                <p>{user?.displayName}</p>
+                <p>{user?.email}</p>
+                <p></p>
+            </div>
+            <div>
+                <p></p>
+            </div>
         </div>
     );
 };
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -27,12 +27,13 @@ const useAxiosSecure = () => {
         return response;
     }, async (error) => {
         const status = error.response.status;
+        console.log(status)
         // console.log('status error in the interceptor', status);
         // for 401 or 403 logout the user and move the user to the login
-        if (status === 401 || status === 403) {
-            await logOut();
-            navigate('/login');
-        }
+        // if (status === 401 || status === 403) {
+        //     await logOut();
+        //     navigate('/login');
+        // }
         return Promise.reject(error);
     })
 
