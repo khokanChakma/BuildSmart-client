989c38e7abd11444591e403ca24b80fb6b021d5b
khokan <khokancrmt@gmail.com>
2025-01-20 20:45:33 +0600

    update search function

--- src/pages/Apartments.jsx
+++ src/pages/Apartments.jsx
@@ -7,7 +7,7 @@ import { useLoaderData } from "react-router-dom";
 
 const Apartments = () => {
     const axiosPublic = useAxiosPublic();
-    // const [apartments, setApartments] = useState([]);
+    const [apartments, setApartments] = useState([]);
     const cardPerPage = 6;
     const [currentPage, setCurrentPage] = useState(0)
     const { count } = useLoaderData();
@@ -15,35 +15,18 @@ const Apartments = () => {
 
     const [minRent, setMinRent] = useState('');
     const [maxRent, setMaxRent] = useState('');
-    const [filteredApartments, setFilteredApartments] = useState([]);
 
-    console.log(filteredApartments)
+    console.log(maxRent,minRent)
 
     const pages = [...Array(numberOfPage).keys()];
 
     useEffect(() => {
         const fetchAllJobs = async () => {
-            const { data } = await axiosPublic.get(`/apartments?page=${currentPage}&size=${cardPerPage}`);
-            setFilteredApartments(data);
+            const { data } = await axiosPublic.get(`/apartments?page=${currentPage}&size=${cardPerPage}&min=${minRent}&max=${maxRent}`);
+            setApartments(data);
         }
         fetchAllJobs();
-    }, [cardPerPage, currentPage,setFilteredApartments])
-
-
-    
-
-    const handleSearch = () => {
-        const min = parseInt(minRent) || 0;
-        const max = parseInt(maxRent) || Infinity;
-
-        const filtered = setFilteredApartments.filter(
-            apartment => apartment.rent >= min && apartment.rent <= max
-        );
-        setFilteredApartments(filtered);
-    };
-
-
-
+    }, [cardPerPage, currentPage,setApartments])
 
     const handlePrevPage = () => {
         if (currentPage > 0) {
@@ -60,27 +43,9 @@ const Apartments = () => {
     return (
         <div>
             <div className="my-10">
-                <div>
+                <div className="">
                     <h2 className="text-2xl font-semibold text-center mb-4">Search By Rent</h2>
-                    {/* <div className="md:flex gap-4 justify-center">
-                        <input
-                             onChange={(e) => setSearch(e.target.value)}
-                            type="text"
-                            placeholder="Min rent"
-                            className="input input-bordered w-full max-w-xs"
-                        />
-                        <input
-                             onChange={(e) => setSearch(e.target.value)}
-                            type="text"
-                            placeholder="Max rent"
-                            className="input input-bordered w-full max-w-xs"
-                        />
-
-                        <button className='px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
-                            Search
-                        </button>
-                    </div> */}
-                    <div className="flex items-center space-x-4">
+                    <div className="flex items-center justify-center space-x-4">
                         <input
                             type="number"
                             placeholder="Min Rent"
@@ -95,13 +60,13 @@ const Apartments = () => {
                             onChange={(e) => setMaxRent(e.target.value)}
                             className="input input-bordered"
                         />
-                        <button onClick={handleSearch} className="btn btn-primary">Search</button>
+                        <button className="btn btn-primary">Search</button>
                     </div>
                 </div>
             </div>
             <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
                 {
-                    filteredApartments.map(apartment => <ApartmentCard key={apartment._id} apartment={apartment}></ApartmentCard>)
+                    apartments.map(apartment => <ApartmentCard key={apartment._id} apartment={apartment}></ApartmentCard>)
                 }
             </div>
             <div className="text-center mb-10 space-x-4">
